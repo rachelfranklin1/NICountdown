270120b4cb16465b491ea18fe55c55dfed617021
rachelfranklin1 <rachel.franklin@marvelapp.com>
2021-04-11 17:48:36 +0100

    more styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,14 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import { useCountdown } from "../hooks/useCountdown";
+import "@fontsource/open-sans";
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  // padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
   display: "flex",
   alignItems: "center",
   justifyContent: "space-evenly",
   height: "100vh",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-const headingAccentStyles = {
-  color: "#663399",
 };
 
 // markup
@@ -27,37 +17,66 @@ const IndexPage = () => {
   return (
     <>
       <GlobalStyle />
+      {/* <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <LeftHeader>Countdown to</LeftHeader>
+        <RightHeader>the sesh</RightHeader>
+      </div> */}
       <main style={pageStyles}>
         <title>Countdown to the sesh</title>
         <LeftPanel>
-          <h1 style={headingStyles}>Countdown to the sesh</h1>
-          <span style={headingAccentStyles}>Currently you can </span>
-          <span role="img" aria-label="Party popper emojis">
-            meet up to 6 people from two households
-          </span>
+          <h1>Currently you can meet up to 6 people from two households</h1>
         </LeftPanel>
         <RightPanel>
-          <h1>days {timeLeft.days}</h1>
-          <h1>hours {timeLeft.hours}</h1>
-          <h1>minutes {timeLeft.minutes}</h1>
-          <h1>seconds {timeLeft.seconds}</h1>
+          <span style={{ display: "flex" }}>
+            <h1>{timeLeft.days} days</h1>
+            <h1>{timeLeft.hours} hours</h1>
+          </span>
+          <span>
+            <h1>{timeLeft.minutes} minutes</h1>
+            <h1>{timeLeft.seconds} seconds</h1>
+          </span>
         </RightPanel>
       </main>
     </>
   );
 };
-
+const LeftHeader = styled.header`
+  width: 100%;
+  height: 50px;
+`;
+const RightHeader = styled.header`
+  width: 100%;
+  height: 50px;
+`;
 const LeftPanel = styled.div`
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 50%;
+  padding: 0 5%;
   background-color: white;
   color: black;
   height: 100%;
+  font-family: "Open Sans";
 `;
 const RightPanel = styled.div`
   width: 50%;
+  padding: 0 5%;
   background-color: black;
   color: white;
   height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-family: "Open Sans";
 `;
 
 const GlobalStyle = createGlobalStyle`
